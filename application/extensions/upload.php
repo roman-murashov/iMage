c7f8c8b2a4819c64fa9bae4513ead8db6c307c1f
tomasci <zivgta@gmail.com>
2016-12-27 23:24:43 +0300

    iMage 2.0.0.1 - Fix
    - скрипт загрузки проверяет, является ли файл картинкой.

--- application/extensions/upload.php
+++ application/extensions/upload.php
@@ -13,24 +13,28 @@
     $spoga = new spogaCrypt;
 
     if (!empty($_FILES)) {
-        if ($_FILES['file']['tmp_name'] == '') {
-            echo 'Файл не был передан.';
-        } else {
-            $now = date('Y_m_d');
-            $userfolder = ROOT . DS . $ae->site()['upload_folder'] . DS . $now . DS;
+        if(preg_match("/(gif|jpg|jpeg|png|GIF|JPG|JPEG|PNG)$/", $_FILES['file']['name'])) {
+            if ($_FILES['file']['tmp_name'] == '') {
+                echo 'Файл не был передан.';
+            } else {
+                $now = date('Y_m_d');
+                $userfolder = ROOT . DS . $ae->site()['upload_folder'] . DS . $now . DS;
 
-            if (!file_exists($userfolder)) {
-                mkdir($userfolder, 0777);
+                if (!file_exists($userfolder)) {
+                    mkdir($userfolder, 0777);
+                }
+
+                $userfile = $_FILES['file']['tmp_name'];
+                $userfileext = substr(strrchr($_FILES['file']['name'],'.'), 1);
+
+                $hash = $spoga->openssl_myrand(6);
+                $target = $userfolder . $hash . '.' . $userfileext;
+                $target_res = DS . $ae->site()['upload_folder'] . DS . $now . DS . $hash . '.' . $userfileext;
+                move_uploaded_file($userfile, $target);
+
+                echo 'http://' . DOMAIN . $target_res;
             }
-
-            $userfile = $_FILES['file']['tmp_name'];
-            $userfileext = substr(strrchr($_FILES['file']['name'],'.'), 1);
-
-            $hash = $spoga->openssl_myrand(6);
-            $target = $userfolder . $hash . '.' . $userfileext;
-            $target_res = DS . $ae->site()['upload_folder'] . DS . $now . DS . $hash . '.' . $userfileext;
-            move_uploaded_file($userfile, $target);
-
-            echo 'http://' . DOMAIN . $target_res;
+        } else {
+            echo "Загружать можно только картинки!";
         }
     }
